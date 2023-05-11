f77ebb4f42f6b62e899c91c7e4e5f134fa216edd
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-11 06:42:18 +0000

    [add]show & edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+  get 'books/edit/:id' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

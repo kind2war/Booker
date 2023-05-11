0d5802545d83f19ff5870e90c3275e0292d73df9
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-11 08:21:15 +0000

    [add]edit & delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit/:id' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  resources :books
 end

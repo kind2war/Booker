9547684098c428e0edde6a7ab6ae322502792418
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-14 23:44:15 +0000

    [Fin.]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,21 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+ # def new
+ # @book = Book.new
+ # end
 
   def create
-       @book = Book.new(book_params)
+    @book = Book.new(book_params)
     if @book.save
-       redirect_to book_path(book.id)
+      flash[:notice]="Book was successfully created !"
+      redirect_to book_path(@book.id)
     else
-       render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def index
+    @book  = Book.new
     @books = Book.all
   end
 
@@ -25,11 +28,12 @@ class BooksController < ApplicationController
   end
 
   def update
-       @book = Book.find(params[:id])
-    if @book.update
-       redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated !"
+      redirect_to book_path(@book.id)
     else
-       render :new
+      render :edit
     end
   end
 
